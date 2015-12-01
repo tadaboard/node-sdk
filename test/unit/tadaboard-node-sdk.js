490f0af6992eab28938273d91341073138082b35
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2015-12-01 09:56:33 -0800

    Add simple test for TB.response

--- test/unit/tadaboard-node-sdk.js
+++ test/unit/tadaboard-node-sdk.js
@@ -1,4 +1,9 @@
 import TB from '../../src/tadaboard-node-sdk';
 
 describe('TB', () => {
+  describe('response', () => {
+    it('is a function', () => {
+      expect(TB.response).to.be.a('function');
+    });
+  });
 });
