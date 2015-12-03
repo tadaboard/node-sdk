e525574c1d3385cdea6fcc5f07c32a55b41197ed
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2015-12-03 09:23:05 -0800

    Improved example

--- examples/blog/random_dashboard/index.js
+++ examples/blog/random_dashboard/index.js
@@ -10,12 +10,12 @@ function handleRequest(request, response){
   });
 
   request.on('end', function() {
-    var tb = new TB.response({ requestBody: body, queryDefault: {} });
-    tb.widget('1', { title: 'Total users', value: _.random(300,1000), variation: _.random(-10, 10, true) });
-    tb.widget('2', { title: 'Signups', value: _.random(5,30), variation: _.random(-10, 10, true) });
-    tb.widget('3', { title: 'Active', value: _.random(40,300), variation: _.random(-10, 10, true) });
-    tb.widget('4', { title: 'Paying', value: _.random(0,300), variation: _.random(-10, 10, true) });
-    tb.widget('5', { title: 'Lost', value: _.random(10,40), variation: _.random(-10, 10, true) });
+    var tb = new TB.response({ requestBody: body, queryDefault: {metric: '1'}});
+    tb.widget('1', {id: '1', title: 'Total users', value: _.random(300,1000), variation: _.random(-10, 10, true)});
+    tb.widget('2', {id: '2', title: 'Signups', value: _.random(5,30), variation: _.random(-10, 10, true)});
+    tb.widget('3', {id: '3', title: 'Active', value: _.random(40,300), variation: _.random(-10, 10, true)});
+    tb.widget('4', {id: '4', title: 'Paying', value: _.random(0,300), variation: _.random(-10, 10, true)});
+    tb.widget('5', {id: '5', title: 'Lost', value: _.random(10,40), variation: _.random(-10, 10, true)});
     tb.widget('6', _.map(
         _.range(Date.now() - 30 * 24 * 60 * 60 * 1000, Date.now(), 24 * 60 * 60 * 1000)
       , function(t) {
