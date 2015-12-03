51455363be7cba0c3aeaae95cc0f371d521e22d9
Alessandro Tagliapietra <tagliapietra.alessandro@gmail.com>
2015-12-03 09:34:22 -0800

    Fix mispelled package name

--- test/setup/node.js
+++ test/setup/node.js
@@ -1,7 +1,7 @@
 global.chai = require('chai');
 global.sinon = require('sinon');
 global.chai.use(require('sinon-chai'));
-global.chai.use(require('chai-json-schema'));
+global.chai.use(require('chai3-json-schema'));
 
 require('babel-core/register');
 require('./setup')();
